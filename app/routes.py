362c3431de3645d08ebbf53f7e1f0095f3e2d695
takhat <takhat014@gmail.com>
2022-11-04 10:00:38 -0700

    refactored route: create_book, used from_dict() inside create_book

--- app/routes.py
+++ app/routes.py
@@ -31,10 +31,7 @@ def read_all_books():
 @books_bp.route("", methods=["POST"])
 def create_book():
         request_body = request.get_json()
-        new_book = Book(
-            title = request_body["title"],
-            description = request_body["description"]
-        )
+        new_book = Book.from_dict(request_body)
         db.session.add(new_book)
         db.session.commit()
         return make_response(jsonify(f"Book {new_book.title} successfully created with id: {new_book.id}"), 201)
